8eac616a0ae66bc39d7e6ef3a8da0f84e040ce23
John James Jacoby <johnjamesjacoby@me.com>
2016-03-22 14:07:38 -0500

    Bump Term Meta UI to 0.1.8.

--- includes/class-wp-term-meta-ui.php
+++ includes/class-wp-term-meta-ui.php
@@ -9,7 +9,7 @@
  * and handle the sanitization & saving of values.
  *
  * @since 0.1.1
- * @version 0.1.7
+ * @version 0.1.8
  *
  * @package Plugins/Terms/Metadata/UI
  */
@@ -112,7 +112,7 @@ class WP_Term_Meta_UI {
 		$this->file       = $file;
 		$this->url        = plugin_dir_url( $this->file );
 		$this->path       = plugin_dir_path( $this->file );
-		$this->basename   = plugin_basename( $this->file );		
+		$this->basename   = plugin_basename( $this->file );
 		$this->fancy      = apply_filters( "wp_fancy_term_{$this->meta_key}", true );
 
 		// Only look for taxonomies if not already set
@@ -231,10 +231,18 @@ class WP_Term_Meta_UI {
 	 */
 	public function edit_tags() {
 
+		// Bail if taxonomy does not include colors
+		if ( empty( $GLOBALS['taxnow'] ) || ! in_array( $GLOBALS['taxnow'], $this->taxonomies, true ) ) {
+			return;
+		}
+
 		// Enqueue javascript
-		add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_scripts' ) );
-		add_action( 'admin_head',            array( $this, 'help_tabs'       ) );
-		add_action( 'admin_head',            array( $this, 'admin_head'      ) );
+		add_action( 'admin_head-term.php',               array( $this, 'help_tabs'       ) );
+		add_action( 'admin_head-edit-tags.php',          array( $this, 'help_tabs'       ) );
+		add_action( 'admin_head-term.php',               array( $this, 'admin_head'      ) );
+		add_action( 'admin_head-edit-tags.php',          array( $this, 'admin_head'      ) );
+		add_action( 'admin_print_scripts-term.php',      array( $this, 'enqueue_scripts' ) );
+		add_action( 'admin_print_scripts-edit-tags.php', array( $this, 'enqueue_scripts' ) );
 
 		// Quick edit
 		add_action( 'quick_edit_custom_box', array( $this, 'quick_edit_meta' ), 10, 3 );
@@ -345,6 +353,17 @@ class WP_Term_Meta_UI {
 	 * @since 0.1.1
 	 */
 	public function ajax_update() {}
+
+	/**
+	 * Return the formatted output for the column row
+	 *
+	 * @since 0.1.2
+	 *
+	 * @param string $meta
+	 */
+	protected function format_output( $meta = '' ) {
+		return esc_html( $meta );
+	}
 
 	/**
 	 * Return the taxonomies used by this plugin
